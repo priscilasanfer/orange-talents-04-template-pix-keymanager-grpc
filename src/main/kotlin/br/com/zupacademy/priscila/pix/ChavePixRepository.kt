074095ddfb43da4d17aade6d663270c5d215d2b1
Priscila Ferreira <priscilasanfer@gmail.com>
2021-06-11 11:08:26 -0300

    021-listando-todas-as-chaves-pix-do-cliente

--- src/main/kotlin/br/com/zupacademy/priscila/pix/ChavePixRepository.kt
+++ src/main/kotlin/br/com/zupacademy/priscila/pix/ChavePixRepository.kt
@@ -1,6 +1,5 @@
 package br.com.zupacademy.priscila.pix
 
-import br.com.zupacademy.priscila.pix.carrega.ChavePixInfo
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
 import java.util.*
@@ -10,4 +9,5 @@ interface ChavePixRepository : JpaRepository<ChavePix, Long> {
     fun existsByChave(chave: String?): Boolean
     fun findByPixId(pixId: UUID): Optional<ChavePix>
     fun findByChave(chave: String): Optional<ChavePix>
+    fun findAllByClientId(clientId: UUID): List<ChavePix>
 }
